fdb795c3a9ecc0b9864265c904071f57e3a03aa1
Sean <sjgallen@gmail.com>
2017-03-01 16:19:59 -0600

    cleaned up the js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var startIt = function() {
   var screen = window.screen;
   var pageWidth = pageHolder.width = screen.width;
   var pageHeight = pageHolder.height = screen.height;
-  var pageArray = Array(256).join(1).split(''), context = pageHolder.getContext('2d');
-  var math = Math;
+  var pageArray = Array(256).join(1).split('');
+  var context = pageHolder.getContext('2d');
 
   setInterval(function(){
     context.fillStyle='rgba(0,0,0,0.05)';
     context.fillRect(0,0,pageWidth,pageHeight);
     context.fillStyle='rgba(0,255,0,1)';
     pageArray = pageArray.map(function(v,i) {
-      randomNum = math.random();
-      context.fillText(String.fromCharCode(math.floor(2720+randomNum*33)),i*10,v);
+      randomNum = Math.random();
+      context.fillText(String.fromCharCode(Math.floor(2720+randomNum*33)),i*10,v);
       v+=10;
       return v>768+randomNum*1e4?0:v
     }) },33)
